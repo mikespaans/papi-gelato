89c9e5abd40a80baa1cee3f8654388dc6afbd405
Mike Spaans <99069233@mydavinci.nl>
2021-11-01 14:02:52 +0100

    papi-gelato 2e commit

--- Papi_Gelato.py
+++ Papi_Gelato.py
@@ -1,3 +1,9 @@
+def SorrySnapIkNiet():
+    print ("Sorry, dat snap ik niet...")
+def OpniewVragenStap1False():
+    OpnieuwVragenStap1 = "False"
+def OpnieuwVragenStap2False():
+    OpnieuwVragenStap2 = "False"
 OpnieuwVragenStap1 = "True" 
 OpnieuwVragenStap2 = "True"
 OpnieuwVragenStap3 = "True"
@@ -5,35 +11,39 @@ print ("Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille
 #Stap 1
 while OpnieuwVragenStap1 == "True":
     HoeveelBolletjes = int(input("Hoeveel bolletjes wilt u? "))
+    OpnieuwVragenStap3 = "True"
     
     if HoeveelBolletjes <= 8 and HoeveelBolletjes >= 4:
-        OpnieuwVragenStap1 = "False"
+        OpniewVragenStap1False()
         print ("Dan krijgt u van mij een bakje met",HoeveelBolletjes,"bolletjes")
         HoorntjeOfBakje = 'Bakje'
 
     elif HoeveelBolletjes <= 3:
-        OpnieuwVragenStap1 = "False"
+        OpniewVragenStap1False()
         #Stap 2
         while OpnieuwVragenStap2 == "True":
             BakjeOfHoorntje = input("Wilt u deze "+ str(HoeveelBolletjes) +  " bolletje(s) in A) een hoorntje of B) een bakje? ")
             if BakjeOfHoorntje == ("A" or "B"):
                 OpnieuwVragenStap2 = "False"
+                
                 if BakjeOfHoorntje == "A":
                     HoorntjeOfBakje = 'hoorntje'
                 else:
                     HoorntjeOfBakje = 'bakje'
             else:
-                print ("Sorry, dat snap ik niet...")
+                SorrySnapIkNiet()
     elif HoeveelBolletjes > 8:
         print ("Sorry, zulke grote bakken hebben we niet")
         
     else:
-        print ("Sorry, dat snap ik niet...")
+        SorrySnapIkNiet()
                 #Stap 3
     while OpnieuwVragenStap3 == "True":
         NogMeerBestellen = input("Hier is uw " + HoorntjeOfBakje + " met "+ str(HoeveelBolletjes) +" bolletjes. Wilt u nog meer bestellen? (J/N) ")
+        
         if NogMeerBestellen == "J":
             OpnieuwVragenStap1 = "True"
+            OpnieuwVragenStap2 = "True"
             OpnieuwVragenStap3 = "False"
 
         elif NogMeerBestellen == "N":
@@ -41,10 +51,4 @@ while OpnieuwVragenStap1 == "True":
             OpnieuwVragenStap3 = "False"
 
         else:
-            print ("Sorry dat snap ik niet")
-            
-
-        
-    
-
-   
+            SorrySnapIkNiet()
